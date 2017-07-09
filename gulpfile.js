f6bc29dbfc58f4506fc90422e0f5013bfbf601f5
roman <opt1m1st23@yandex.ru>
2017-07-09 08:28:12 +0500

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require("gulp"),
-		browserSync = require('browser-sync');
+var gulp 				= require("gulp"),
+		browserSync = require('browser-sync'),
+		less				=	require('gulp-less');
 
 gulp.task('server', function () {
 	browserSync({
@@ -10,12 +11,19 @@ gulp.task('server', function () {
 	});
 });
 
-gulp.task('watch', function () {
+gulp.task('watch',['server', 'less'], function () {
 	gulp.watch([
 		'app/*.html',
 		'app/js/**/*.js',
-		'app/css/**/*.css'
-		]).on('change', browserSync.reload);
+		'app/css/**/*.css']).on('change', browserSync.reload);
+	gulp.watch(
+		'app/css/*.less', ['less']).on('change', browserSync.reload);
+});
+
+gulp.task('less', function(){
+	return gulp.src('app/css/*.less')
+				.pipe(less())
+				.pipe(gulp.dest('app/css/'));
 });
 
 gulp.task('default', ['server', 'watch']);
